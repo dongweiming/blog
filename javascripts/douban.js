5904c24522a874afc3947e99ae6fae73547ff603
dongweiming <ciici1234@hotmail.com>
2015-10-24 21:28:57 +0800

    Site updated at 2015-10-24 13:28:57 UTC

---
+++ javascripts/douban.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="http://www.douban.com/service/badge/{{ site.douban_user }}/?select=random&amp;n=10&amp;columns=2&amp;picsize=medium&amp;hidelogo=yes&amp;hideself=yes&amp;cat=book" ></script> 
